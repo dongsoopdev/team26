851bf02be5e0abfcb6d6e6e715ed6c966d7c1bba
soumunda8 <soumunda8@naver.com>
2023-12-08 10:58:35 +0900

    [231208 ny] 리캡챠 회원가입, 로그인 적용

---
+++ src/main/java/com/edutech/team26/component/LoginFailHandler.java
@@ -0,0 +1,29 @@
+package com.edutech.team26.component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Log4j2
+public class LoginFailHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+        log.info("------------------------------- error -------------------------------");
+        log.info(exception);
+
+        setDefaultFailureUrl("/login?error=true");
+        super.onAuthenticationFailure(request, response, exception);
+
+    }
+
+}
