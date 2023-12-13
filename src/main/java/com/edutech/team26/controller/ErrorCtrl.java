ac86ab5c97fb295aa8415bdbc1557030479c309f
soumunda8 <soumunda8@naver.com>
2023-12-13 20:19:01 +0900

    [231213 ny] 에러페이지 처리

--- src/main/java/com/edutech/team26/controller/ErrorCtrl.java
+++ src/main/java/com/edutech/team26/controller/ErrorCtrl.java
@@ -5,16 +5,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorCtrl implements ErrorController {
 
     @GetMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object status02 = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
         if (status != null) {
             int statusCode = Integer.valueOf(status.toString());
+            model.addAttribute("statusCode", statusCode);
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error/notFoundError";
             } else {
